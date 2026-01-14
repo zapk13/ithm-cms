d688c9ac493c0bb4caa5674f998821bb86a3ad6f
zapk13 <zapk13@gmail.com>
2026-01-14 12:59:55 +0500

    Update production DB password

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return [
     'port' => 3306,
     'database' => 'ithmpwus_ithm_cms',
     'username' => 'ithmpwus_ztdcp',
-    'password' => 'ithm_tdcp_cms;44532',
+    'password' => 'u2AIg*F4?~.+o;e!',
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
